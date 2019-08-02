b0a45b2b846ee73702c8cb86d2080ea5098dff38
msmiah <msmiah@miners.utep.edu>
2019-08-02 01:37:48 -0600

    First solver commit

--- src/TestGameResult/ExperimentsGame.java
+++ src/TestGameResult/ExperimentsGame.java
@@ -6,7 +6,6 @@ import solver.ApproximationSolver;
 public class ExperimentsGame {
    public static void main(String[] args) {
 	   
-	  // System.out.println("HoneyFlow");
 	   Game g = new Game();
 	   ApproximationSolver solver = new ApproximationSolver(g);
    }
--- src/solver/ApproximationSolver.java
+++ src/solver/ApproximationSolver.java
@@ -1,32 +1,109 @@
 package solver;
 
+import Utils.Utils;
 import game.Game;
 import game.Game.Action;
 import game.Game.Node;
+import gurobi.GRB;
+import gurobi.GRBEnv;
+import gurobi.GRBException;
+import gurobi.GRBLinExpr;
+import gurobi.GRBModel;
+import gurobi.GRBVar;
 
 public class ApproximationSolver extends ZeroSumGameSolver {
+	GRBModel model;
+	GRBLinExpr objective;
+	GRBLinExpr brConstraints;
+	GRBLinExpr honeyflowConstraints;
+	private int zCnt;
 
 	public ApproximationSolver(Game game) {
 		super(game);
-		CreateSequenceFormVariablesAndConstraints(0);
+		try {
+			initializeDataStructure();
+		} catch (GRBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	private void CreateSequenceFormVariablesAndConstraints(int currentNodeId) {
+	private void initializeDataStructure() throws GRBException {
+		try {
+			GRBEnv env = new GRBEnv("ApproximationSolver.log");
+			 model = new GRBModel(env);
+		} catch (GRBException e) {
+			System.out.println("Error LPSolver(): Gurobi setup failed");
+		}
+		zCnt = 0;
+		objective =  new GRBLinExpr();
+		brConstraints =  new GRBLinExpr();
+		honeyflowConstraints = new GRBLinExpr();
+		CreateSequenceFormVariablesAndConstraints(0,null,null);
+	}
+	private GRBVar twoBinaryVarTinearization(GRBVar var1, GRBVar var2) throws GRBException {
+
+		GRBLinExpr lz1 =  new GRBLinExpr();
+		GRBLinExpr lz2 =  new GRBLinExpr();
+		GRBLinExpr lz3 =  new GRBLinExpr();
+		GRBVar z = model.addVar(0, Utils.MAX_LIMIT_OF_HONEY_FLOW, 0, GRB.CONTINUOUS,   "z" + zCnt);
+
+		/* z-var1 <= 0 */
+		lz1.addTerm(1, z);
+		lz1.addTerm(-1, var1);
+		model.addConstr(lz1, GRB.LESS_EQUAL, 0, "TLZ1" + zCnt);
+		
+
+		/* z-var2 <= 0 */
+		lz1.addTerm(1, z);
+		lz1.addTerm(-1, var2);
+		model.addConstr(lz1, GRB.LESS_EQUAL, 0, "TLZ2" + zCnt);
+		
+
+		/*Z-Var1-Var2+1 <= 0*/
+		lz3.addTerm(1, z);
+		lz3.addTerm(-1, var1);
+		lz3.addTerm(-1, var2);
+		model.addConstr(lz3, GRB.LESS_EQUAL, -1, "TLZ3" +zCnt);
+		
+		return z;
+		
+		
+	}
+	private void CreateSequenceFormVariablesAndConstraints(int currentNodeId, GRBLinExpr parentVariable,
+			GRBVar childVariable) throws GRBException {
 		Node node = game.getNodeById(currentNodeId);
-		if (null == node || node.isLeaf()) {
+		if (node.isLeaf()) {
+			double value = node.getValue();
+
+			
+			// primalConstraints.put(node.getInformationSet(),
+			System.out.println(value+ ": par :" +parentVariable + " : child" + childVariable);
 			return;
 		}
 
 		for (Action action : node.getActions()) {
 			if (node.getPlayer() == 1) {
-				CreateSequenceFormVariablesAndConstraints(action.getChildId());
+				GRBLinExpr sumOfBinaryVar = new GRBLinExpr();
+				GRBLinExpr sum = new GRBLinExpr();
+				for (int i = 0; i <= Utils.MAX_LIMIT_OF_HONEY_FLOW; i++) {
+					GRBVar v = model.addVar(0, 1, 0, GRB.BINARY,
+							"node:" + node.getNodeId() + "  action:" + action.getName());
+					sumOfBinaryVar.addTerm(i, v);
+					sum.addTerm(1,v);
+
+				}
+				honeyflowConstraints.add(sumOfBinaryVar);
+				model.addConstr(sum, GRB.EQUAL, 1, "P1" + node.getNodeId());
+				CreateSequenceFormVariablesAndConstraints(action.getChildId(), sumOfBinaryVar, childVariable);
 
 			} else if (node.getPlayer() == 2) {
-				CreateSequenceFormVariablesAndConstraints(action.getChildId());
+				GRBVar v = model.addVar(0, 1, 0, GRB.BINARY,
+						"node:" + node.getNodeId() + "  action:" + action.getName());
+				CreateSequenceFormVariablesAndConstraints(action.getChildId(), parentVariable, v);
 			}
 		}
-
 	}
 
 	@Override
